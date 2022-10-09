abee6798e02ab49bbcee59583420948aad151131
selim-khouaja <selimkhouaja@gmail.com>
2022-10-09 18:35:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char* argv[])
 
   {
         std::cout << "A better, new initial message" << std::endl;
+        std::cout << "This is the new change made at exercise 7.2" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
